123b4b40dab418daecf06b51422fa73ce7e85dbe
JonSeven <mazahaker5555@yandex.ru>
2024-05-19 23:22:55 +0300

    initial commit for structure of project

---
+++ src/main/java/com/github/jon7even/bankingservice/entity/UserEntity.java
@@ -0,0 +1,19 @@
+package com.github.jon7even.bankingservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Класс описывающий сущность пользователя
+ *
+ * @author Jon7even
+ * @version 1.0
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+}
